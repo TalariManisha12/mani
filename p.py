bec07638a1e31283c90b1ce25084def19be9bd25
talari manisha <talarimanisha2020@gmail.com>
2024-06-09 13:46:45 +0530

    adding copy 1

---
+++ p.py
@@ -0,0 +1,62 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "222769db-6e01-4fce-82cd-2c1e5adbbaa7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "enter degree celcius: 34\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "degree cel: 34\n",
+      "c to f is: 93.2\n"
+     ]
+    }
+   ],
+   "source": [
+    "ct=int(input(\"enter degree celcius:\"))\n",
+    "print(\"degree cel:\",ct)\n",
+    "ft=1.8*ct+32\n",
+    "print(\"c to f is:\",ft)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e14cac25-10f0-425f-845c-7322a68d8b28",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
